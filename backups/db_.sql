ca305083b3ff7bc317d7cc4e634e45b5f9e5cfa0
Nik <polovynkok@gmail.com>
2025-09-09 20:59:32 +0300

    change backup settings

---
+++ backups/db_.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict JR3VFD43YVpX0PgHsjChQP8oZtcovXaTvM1OAa7ShIh3hwrfmj3l1Cc1RLb651o
+
+-- Dumped from database version 15.14 (Debian 15.14-1.pgdg13+1)
+-- Dumped by pg_dump version 15.14 (Debian 15.14-1.pgdg13+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict JR3VFD43YVpX0PgHsjChQP8oZtcovXaTvM1OAa7ShIh3hwrfmj3l1Cc1RLb651o
+
